dd36463c39ad76c1d6ef9cff475e91541290622f
sparsh120911 <83496970+sparsh120911@users.noreply.github.com>
2021-09-18 13:37:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function searching()
+
+var Guestname =document.getElementById("name1").value;
+//use the push function as- name_of_people.push(Guestname);
+
+document.getElementById(" ").style.display="block";
+ //display the name in the id= sort_button
+
+var names_of_people    //add the sort function
+// .the sort function is .sort();
